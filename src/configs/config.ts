51ef2a6291bcb32aefe24338a8ac7d8bc20d8b16
Alifoxy <nightfoxy666@gmail.com>
2024-05-02 19:57:58 +0300

    homework2

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -9,6 +9,9 @@ export const config = {
 
     HASH_ROUNDS: Number(process.env.HASH_ROUNDS),
 
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    SENDGRID_FROM_EMAIL: process.env.SENDGRID_FROM_EMAIL,
+
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
     JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN,
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
